3872b3d0e8f0a7825e36e3aa1fbed4e81e5fb1d2
Krzysztof Kabat <odrinwhite@gmail.com>
2025-12-31 10:32:38 +0000

    chore: fix spawner preview in editor, added export config

--- entities/network/player_spawner/player_spawner.gd
+++ entities/network/player_spawner/player_spawner.gd
@@ -3,18 +3,32 @@ class_name PlayerSpawner
 
 extends Node3D
 
-@export var PLAYER_SCENE: PackedScene;
+@export var PLAYER_SCENE: PackedScene:
+	set(value):
+		PLAYER_SCENE = value
+		if Engine.is_editor_hint():
+			_update_editor_preview()
 
 func _ready() -> void:
 	if Engine.is_editor_hint():
-		if PLAYER_SCENE == null:
-			return
-		spawn_player(1)
+		_update_editor_preview()
 		return
 
 	NetworkHandler.on_peer_connected.connect(spawn_player)
 	ClientNetworkGlobals.on_id_assigned.connect(spawn_player)
 	ClientNetworkGlobals.on_remote_id_assigned.connect(spawn_player)
+
+
+func _update_editor_preview() -> void:
+	for child in get_children():
+		child.queue_free()
+	
+	if PLAYER_SCENE == null:
+		return
+	
+	var player = PLAYER_SCENE.instantiate()
+	player.name = "PlayerPreview"
+	add_child(player)
 
 
 func spawn_player(id: int) -> void:
